29f87f3aa0eb6bd65bbde8f9aa3ee41cae8acb62
Douglas Tardioli <douglastardioli48@gmail.com>
2024-04-04 20:44:10 -0300

    finish backend and initial frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import react from "react"
+
+function App() {
+
+
+  return (
+    <main className="w-screen h-screen bg-slate-950">
+      <h1 className="text-zinc-50">Hellooo</h1>
+    </main>
+  )
+}
+
+export default App
